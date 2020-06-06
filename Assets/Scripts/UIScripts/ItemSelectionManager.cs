6fdb1696a032b0028967c2591b4f7593d7ee0420
simppa <simppabauer@gmail.com>
2020-06-06 20:04:57 +0300

    Add ItemSelectionManager for the UI

--- Assets/Scripts/UIScripts/ItemSelectionManager.cs
+++ Assets/Scripts/UIScripts/ItemSelectionManager.cs
@@ -2,21 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ItemSelectionManager : MonoBehaviour
 {
 
-    GameObject itemSelectionTile;
+    public GameObject itemSelectionTile;
     private int itemCount;
+    private List<GameObject> items = new List<GameObject>();
     void Start()
     {
         itemCount = PrefabDictionary.Instance.buildingBlockDictionary.Count;
-
-
+        InstantiateItems();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InstantiateItems(){
+        for (int i = 0; i < itemCount; i++)
+        {
+            GameObject gameObject;
+            gameObject = Instantiate(itemSelectionTile,transform.parent);
+            items.Add(gameObject);
+            gameObject.transform.SetParent(this.transform);
+        }
     }
 }
